3ec59cec28c372552169005529d9349918187bdf
taufiqelahi <taufiqelahiiceiu@gmail.com>
2024-02-04 23:07:54 +0600

    repeat keyword

--- Hello.java
+++ Hello.java
@@ -4,6 +4,11 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("hello world ");
+        String a="*";
+  
+        for(int i=0; i<4;i++){
+            
+          System.out.println(a.repeat(i));
+        }
     }
 }
