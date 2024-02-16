41df7a70ffb9fe6fba136018fded06333d05636e
taufiqelahi <taufiqelahiiceiu@gmail.com>
2024-02-16 16:06:29 +0600

    hollow rectengle pattern

---
+++ HollowPattern.java
@@ -0,0 +1,20 @@
+public class HollowPattern {
+    public static void main(String[] args) {
+        int m=5;
+        int n=4;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(i==1||i==n||j==1||j==m){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+
+                }
+               
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
