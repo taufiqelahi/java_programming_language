9b14336ca71ff5a07a5b7dc948ecb87c1890e2b4
taufiqelahi <taufiqelahiiceiu@gmail.com>
2024-02-16 15:15:32 +0600

    learning parttren in java

---
+++ Pattren.java
@@ -0,0 +1,13 @@
+public class Pattren {
+    public static void main(String[] args) {
+        String a="*";
+        for (int i = 0; i < 4; i++) {
+        //   for (int j=0;j<5;j++){
+        //     System.out.print(a);
+        //   }
+          System.out.println(a.repeat(5));
+        
+        }
+    }
+    
+}
