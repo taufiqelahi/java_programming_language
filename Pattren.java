ad8294f2ae1343a92e4ba266bc7cb48bf9b86f04
taufiqelahi <taufiqelahiiceiu@gmail.com>
2024-02-16 15:21:04 +0600

    solid rectangle pattern

--- Pattren.java
+++ Pattren.java
@@ -2,10 +2,10 @@ public class Pattren {
     public static void main(String[] args) {
         String a="*";
         for (int i = 0; i < 4; i++) {
-        //   for (int j=0;j<5;j++){
-        //     System.out.print(a);
-        //   }
-          System.out.println(a.repeat(5));
+          for (int j=0;j<5;j++){
+            System.out.print(a);
+          }
+       System.out.println();
         
         }
     }
