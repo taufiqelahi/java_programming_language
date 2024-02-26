910b7a299e5aae6820c697c707c4776884be9f6b
taufiqelahi <taufiqelahiiceiu@gmail.com>
2024-02-26 10:20:49 +0600

    completed floyd pyramid

---
+++ InversedHalfPyramid.java
@@ -0,0 +1,15 @@
+public class InversedHalfPyramid {
+    public static void main(String[] args) {
+        int n=4;
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=n-i;j++){
+                System.err.print(" ");
+            }
+            for(int j=0;j<=i;j++){
+                System.err.print(j);
+            }
+            System.out.println();
+        }
+    }
+    
+}
