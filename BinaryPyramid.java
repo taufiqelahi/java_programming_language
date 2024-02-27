113c4db22cd5e190cc9ac56e19238fd3a79bffa0
taufiqelahi <taufiqelahiiceiu@gmail.com>
2024-02-27 20:09:35 +0600

    full pyranid undersanting

---
+++ BinaryPyramid.java
@@ -0,0 +1,19 @@
+public class BinaryPyramid {
+public static void main(String[] args) {
+    int n=5;
+    for(int i=1;i<=n;i++) {
+        for(int j=1;j<=i;j++){
+            int r=i+j;
+           
+            if(r%2==0){
+                System.out.print("1");
+            }else{
+                System.out.print("0"); 
+            }
+          
+        }
+        System.out.println();
+    }  
+       
+}
+}
--- InversedHalfPyramid.java
+++ InversedHalfPyramid.java
@@ -2,11 +2,11 @@ public class InversedHalfPyramid {
     public static void main(String[] args) {
         int n=4;
         for(int i=0;i<=n;i++){
-            for(int j=0;j<=n-i;j++){
+            for(int j=0;j<n-i;j++){
                 System.err.print(" ");
             }
-            for(int j=0;j<=i;j++){
-                System.err.print(j);
+            for(int j=0;j<i;j++){
+                System.err.print("*");
             }
             System.out.println();
         }
