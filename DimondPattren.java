23c2474578b3a03ce225bc2cab3fad162265991d
taufiqelahi <taufiqelahiiceiu@gmail.com>
2024-04-06 23:04:24 +0600

    diamond pyramid completed

---
+++ DimondPattren.java
@@ -0,0 +1,29 @@
+public class DimondPattren {
+    public static void main(String[] args) {
+        int rows=4;
+        
+        for(int i=1;i<=rows;i++){
+            for (int j = 1; j <= rows - i; j++) {
+               
+                System.out.print(" ");
+               
+            }
+            for(int j=1; j<=2*i-1;j++){
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+        for(int i=rows;i>=1;i--){
+            for (int j = 1; j <= rows - i; j++) {
+               
+                System.out.print(" ");
+               
+            }
+            for(int j=1;j<=2*i-1;j++){
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+    }
+    
+}
