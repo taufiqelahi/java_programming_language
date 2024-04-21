8bde4cde3d2f790e89281fa5e64a6404e7720119
taufiqelahi <taufiqelahiiceiu@gmail.com>
2024-04-21 21:43:44 +0600

    added functions

---
+++ Pattren/Functions/GivenName.java
@@ -0,0 +1,16 @@
+package Pattren.Functions;
+
+import java.util.Scanner;
+
+public class GivenName {
+    public static void printAGivenName(String name){
+        System.out.println(name);
+    }
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        String username=scanner.nextLine();
+        printAGivenName(username);
+
+    }
+    
+}
