0141840a92058f9e213be1af7b61913feb09fa1b
taufiqelahi <taufiqelahiiceiu@gmail.com>
2024-04-21 21:57:55 +0600

    calculate product

---
+++ Pattren/Functions/MutiplyNumber.java
@@ -0,0 +1,18 @@
+package Pattren.Functions;
+
+import java.util.Scanner;
+
+public class MutiplyNumber {
+    public static int porductOfNumbers(int a, int b){
+        int product=a*b;
+        System.out.println(product);
+        return product;
+            }
+            
+            public static void main(String[] args) {
+                Scanner scanner=new Scanner(System.in);
+                int a= scanner.nextInt();
+               int  b= scanner.nextInt();
+               porductOfNumbers(a, b);
+            }
+}
