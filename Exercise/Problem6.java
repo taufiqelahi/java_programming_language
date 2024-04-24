34fd4834636aa4a822c1e4efb26ba7057861cb2f
taufiqelahi <taufiqelahiiceiu@gmail.com>
2024-04-24 14:51:38 +0600

    created infinty loop usign do while loop

---
+++ Exercise/Problem6.java
@@ -0,0 +1,17 @@
+package Exercise;
+
+public class Problem6 {
+    public static void main(String[] args) {
+        int a=10;
+        do {
+            System.out.println("hello");
+          
+            
+        } while (a%2==0);
+
+
+
+       System.out.println("he");
+    }
+    
+}
