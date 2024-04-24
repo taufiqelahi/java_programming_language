3a666216d45c88ab058bc8b2988575a619fdbaad
taufiqelahi <taufiqelahiiceiu@gmail.com>
2024-04-24 14:34:04 +0600

    calculate circumference of circle

---
+++ Exercise/Problem4.java
@@ -0,0 +1,17 @@
+package Exercise;
+
+import java.util.Scanner;
+
+public class Problem4 {
+    public static void calculatedCircumference(double r){
+double circumfence=2* Math.PI * r;
+String formateCircumfence=String.format("%2f", circumfence);
+System.out.println(formateCircumfence);
+
+    }
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+double r=scanner.nextDouble();
+calculatedCircumference(r);
+    }
+}
