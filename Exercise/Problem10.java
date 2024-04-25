2ef331ef9a5f762a5479e172bb808f6d31aff40c
taufiqelahi <taufiqelahiiceiu@gmail.com>
2024-04-25 09:44:30 +0600

    caculate fibonacci series

---
+++ Exercise/Problem10.java
@@ -0,0 +1,36 @@
+package Exercise;
+
+import java.util.Scanner;
+
+public class Problem10 {
+    public static int calculateFibonacci(int n) {
+
+        if (n <= 1) {
+            return n;
+        } else {
+            int result = 0;
+            int a = 0;
+            int b = 1;
+            for (int i = 2; i <= n; i++) {
+                result = a + b;
+                a = b;
+                b = result;
+
+            }
+            return result;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(calculateFibonacci(i));
+        }
+        scanner.close();
+
+    }
+
+}
