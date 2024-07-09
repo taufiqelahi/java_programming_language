1a492ded9bdd66684d434b80c981a2049c934882
taufiqelahi <taufiqelahiiceiu@gmail.com>
2024-07-09 20:28:54 +0600

    convert decimal into binary

---
+++ BitManupulation/ConvertDecimal.java
@@ -0,0 +1,16 @@
+package BitManupulation;
+
+public class ConvertDecimal {
+    public static void main(String[] args) {
+        String x="1101";
+        double a=0;
+        for (int i=0;i<x.length();i++){
+            if(x.charAt(x.length()-1-i)!='0'){
+                a=a+Math.pow(2, i);
+
+            }
+        }
+        System.out.println(a);
+    }
+    
+}
