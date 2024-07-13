c619618da57fbe9bf5bf0145e540772e89f6c171
taufiqelahi <taufiqelahiiceiu@gmail.com>
2024-07-13 22:44:37 +0600

    addded all

--- BitManupulation/ConvertDecimal.java
+++ BitManupulation/ConvertDecimal.java
@@ -4,6 +4,7 @@ public class ConvertDecimal {
     public static void main(String[] args) {
         String x="1101";
         double a=0;
+        
         for (int i=0;i<x.length();i++){
             if(x.charAt(x.length()-1-i)!='0'){
                 a=a+Math.pow(2, i);
