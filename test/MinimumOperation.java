00d85257d36e8df43baea2ddddc66c66baebc0a6
taufiqelahi <taufiqelahiiceiu@gmail.com>
2024-08-09 00:14:57 +0600

    Added minimum operation

--- test/MinimumOperation.java
+++ test/MinimumOperation.java
@@ -1,22 +1,22 @@
 package test;
 
 public class MinimumOperation {
-   public static void main(String[] args) {
-    
-    int[] nums={3,6,9};
-    int a=0;
+    public static void main(String[] args) {
 
-    for(int i=0;i<nums.length;i++){
-        if(nums[i]%3!=0){
-           if((nums[i]+1)%3==0){
-a=a+1;
-           }else if((nums[i]-1)%3==0){
-a=a+1;
-           }
+        int[] nums = { 3, 6, 9 };
+        int a = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 3 != 0) {
+                if ((nums[i] + 1) % 3 == 0) {
+                    a = a + 1;
+                } else if ((nums[i] - 1) % 3 == 0) {
+                    a = a + 1;
+                }
+            }
         }
+
+        System.out.println(a);
     }
 
-    System.out.println(a);
-   }
-   
 }
