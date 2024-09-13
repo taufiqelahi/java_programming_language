2851d1d2d914dd8d8079b9bd0be93d8675e412a2
taufiqelahi <taufiqelahiiceiu@gmail.com>
2024-09-13 22:00:09 +0600

    complete problem 3

---
+++ Practice/PraticeParttren3.java
@@ -0,0 +1,14 @@
+package Practice;
+
+public class PraticeParttren3 {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+    
+}
