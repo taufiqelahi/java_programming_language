66f9525ceb9659ca356848ae72c796638f856400
taufiqelahi <taufiqelahiiceiu@gmail.com>
2024-09-13 22:00:59 +0600

    complete problem 3

--- Practice/PraticeParttren3.java
+++ Practice/PraticeParttren3.java
@@ -3,7 +3,7 @@ package Practice;
 public class PraticeParttren3 {
     public static void main(String[] args) {
         int n=5;
-        for(int i=1;i<=5;i++){
+        for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
                 System.out.print(j);
             }
