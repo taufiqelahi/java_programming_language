b2eed0da93859b73f3ccd837c3a57dbbc2429b73
taufiqelahi <taufiqelahiiceiu@gmail.com>
2024-09-14 23:26:13 +0600

    complete problem 11

---
+++ Practice/PracticeParttren11.java
@@ -0,0 +1,21 @@
+package Practice;
+
+public class PracticeParttren11 {
+ public static void main(String[] args) {
+    int n=5;
+    int start=1;
+    for(int i=0;i<=n;i++){
+        if ((i%2==0)) {
+            start=1;
+        }else{
+            start=0;
+        }
+        for(int j=0;j<=i;j++){
+            System.out.print(start);
+            start=1-start;
+        }
+        System.out.println();
+    }
+ }
+    
+}
