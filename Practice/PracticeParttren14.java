fa5b5ac893be9159cca2e6c58ac74a4ce9f241a5
taufiqelahi <taufiqelahiiceiu@gmail.com>
2024-09-17 21:47:21 +0600

    completed problem 14

---
+++ Practice/PracticeParttren14.java
@@ -0,0 +1,16 @@
+package Practice;
+
+public class PracticeParttren14 {
+    public static void main(String[] args) {
+        
+        int n=5;
+        for(int i=0;i<n;i++){
+            for(char j='A';j<='A'+i;j++){
+                System.out.print(j);
+              
+            }
+            System.out.println();
+        }
+    }
+    
+}
