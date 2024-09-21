1640dbed9d085417c017c251678511d3cbec3805
taufiqelahi <taufiqelahiiceiu@gmail.com>
2024-09-21 23:49:42 +0600

    solving problem 18

---
+++ Practice/PracticeProblem18.java
@@ -0,0 +1,13 @@
+package Practice;
+
+public class PracticeProblem18 {
+    public static void main(String[] args) {
+        for(int i=0;i<5;i++){
+            for(char j=(char) ('E'-i);j<='E';j++){
+System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+    
+}
