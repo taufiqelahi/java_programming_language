c2ef398e124bc0f83dea4fdcb6a7ccc56fc01a25
taufiqelahi <taufiqelahiiceiu@gmail.com>
2024-09-21 23:15:21 +0600

    complete problem 15

---
+++ Practice/PrcaticeParttren15.java
@@ -0,0 +1,16 @@
+package Practice;
+
+public class PrcaticeParttren15 {
+    public static void main(String[] args) {
+        char es='A';
+        for(int i=0;i<5;i++){
+            for(int j=0;j<=i;j++){
+System.out.print(es);
+            }
+            es ++;
+
+            System.out.println();
+        }
+    }
+    
+}
