b7af6f73a8ede5fae82edfd11f54029c00de47be
taufiqelahi <taufiqelahiiceiu@gmail.com>
2024-09-21 23:08:29 +0600

    complete problem 15

---
+++ Practice/PraticeProblem15.java
@@ -0,0 +1,14 @@
+package Practice;
+
+public class PraticeProblem15 {
+ public static void main(String[] args) {
+    
+    for(int i=5;i>=0;i--){
+        for(char j='A';j<='A'+i;j++){
+System.out.print(j);
+        }
+        System.out.println();
+    }
+ }
+    
+}
