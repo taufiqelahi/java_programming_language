3547600ebdd0285787a55952c2b113ca8f1c8795
taufiqelahi <taufiqelahiiceiu@gmail.com>
2024-09-23 07:35:46 +0600

    completed solved problem 21

---
+++ Practice/PraticeParttren21.java
@@ -0,0 +1,19 @@
+package Practice;
+
+public class PraticeParttren21 {
+public static void main(String[] args) {
+    
+    int n=5;
+    for(int i=1;i<n;i++){
+        for (int j=1;j<n;j++){
+            if(i==1|| j==1||i==n-1||j==n-1){
+                System.out.print("*");
+
+            }else{
+                System.out.print(" ");
+            }
+        }
+        System.out.println();
+    }
+}
+}
