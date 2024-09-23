7128c87245aa662b268b898de40e18abd691b008
taufiqelahi <taufiqelahiiceiu@gmail.com>
2024-09-23 07:15:29 +0600

    complete problem 20

---
+++ Practice/PraticeParttren20.java
@@ -0,0 +1,32 @@
+package Practice;
+
+public class PraticeParttren20 {
+    public static void main(String[] args) {
+        int n=5;
+
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            for(int j=1;j<=2*n-2*i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=n-1;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            for(int j=1;j<=2*n-2*i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
