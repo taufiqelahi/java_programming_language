aa562509092ce99f8847e57fa8d4f893552748da
Taufique A Elahi <91239229+taufiqelahi@users.noreply.github.com>
2024-10-06 10:58:15 +0000

    gcd funtion

--- test/GCDANDLCM.java
+++ test/GCDANDLCM.java
@@ -2,23 +2,18 @@ package test;
 
 public class GCDANDLCM {
     public static void main(String[] args) {
-        int a = 14;
-        int b = 8;
-        int n = 0;
+        int a = 9;
+        int b = 12;
+        int n1 = a;
+        int n2=b;
         int gcd = 1;
         int Lcm = 0;
-        if (a > b) {
-            n = a;
-        } else {
-            n = b;
-        }
-        for (int i = 1; i <= n; i++) {
-            if (a % i == 0 && b % i == 0) {
-                gcd = i;
-            }
-        
-        
-        }
+      while(n1!=0){
+        int temp=n1;
+        n1=n2%n1;
+        n2=temp;
+      }
+      gcd=n2;
         Lcm= (a*b)/gcd;
         System.out.println(gcd + " " + Lcm);
 
