5a988cca90c62f922a1ba93802cd3d061fd14491
taufiqelahi <taufiqelahiiceiu@gmail.com>
2024-11-12 08:44:57 +0600

    undersand hsaing

--- test/Query.java
+++ test/Query.java
@@ -1,5 +1,6 @@
 package test;
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Query {
@@ -14,18 +15,21 @@ public class Query {
         }
 
         //precompute:
+        HashMap<Integer,Integer> h=new HashMap<Integer,Integer>();
         int[] hash = new int[13];
         for (int i = 0; i < n; i++) {
+            h.put(arr[i], h.getOrDefault(arr[i], 0)+1);
             hash[arr[i]] += 1;
         }
-
+System.out.println(h);
         int q;
         q = sc.nextInt();
         while (q-- != 0) {
             int number;
             number = sc.nextInt();
             // fetching:
-            System.out.println(hash[number]);
+            System.out.println(h.getOrDefault(number,0));
         }
+        
     }
 }
